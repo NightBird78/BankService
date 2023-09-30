dfbcb434c7957dbf2707e64463e04dde067f453d
NightBird78 <panbogdan05@gmail.com>
2023-10-01 01:28:02 +0200

    base

---
+++ src/main/java/com/discordshopping/DiscordShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.discordshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordShoppingApplication.class, args);
+    }
+
+}

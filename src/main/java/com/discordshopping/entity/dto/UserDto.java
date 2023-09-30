78fe461c421f1d40cc8e89f1466d6a7682c775de
NightBird78 <panbogdan05@gmail.com>
2023-10-01 01:29:51 +0200

    DTO

---
+++ src/main/java/com/discordshopping/entity/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.discordshopping.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String id;
+    private String nickName;
+    private String firstName;
+    private String lastName;
+    private String email;
+}

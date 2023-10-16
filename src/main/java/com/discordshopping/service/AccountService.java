be71ce1643a60642326c3ae2b54033ff6ce68682
NightBird78 <panbogdan05@gmail.com>
2023-10-16 18:31:28 +0200

    merge

--- src/main/java/com/discordshopping/service/AccountService.java
+++ src/main/java/com/discordshopping/service/AccountService.java
@@ -9,7 +9,17 @@ public interface AccountService {
 
     Optional<UserAccount> findById(String id);
 
+    Optional<UserAccount> findByIDBA(String idba);
+
+    Optional<UserAccount> findByUserId(String id);
+
     boolean create(UserAccount account);
 
     boolean create(AccountDto accountDto);
+
+    void save(UserAccount account);
+
+    Optional<UserAccount> findByEmail(String email);
+
+    boolean transfer(UserAccount from, UserAccount to, String currency, Double amount);
 }
--- src/main/java/com/discordshopping/service/impl/AccountServiceImpl.java
+++ src/main/java/com/discordshopping/service/impl/AccountServiceImpl.java
@@ -1,9 +1,12 @@
 package com.discordshopping.service.impl;
 
+import com.discordshopping.entity.Currency;
 import com.discordshopping.entity.UserAccount;
 import com.discordshopping.entity.dto.AccountDto;
+import com.discordshopping.entity.enums.CurrencyCode;
 import com.discordshopping.service.AccountService;
 import com.discordshopping.service.repository.AccountRepository;
+import com.discordshopping.service.repository.CurrencyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,7 @@ import java.util.UUID;
 public class AccountServiceImpl implements AccountService {
 
     private final AccountRepository accountRepository;
+    private final CurrencyRepository currencyRepository;
 
     @Override
     public Optional<UserAccount> findById(String id) {
@@ -23,9 +27,19 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
+    public Optional<UserAccount> findByIDBA(String idba) {
+        return accountRepository.findByIDBA(idba);
+    }
+
+    @Override
+    public Optional<UserAccount> findByUserId(String id) {
+        return accountRepository.findByUserId(UUID.fromString(id));
+    }
+
+    @Override
     @Transactional
     public boolean create(UserAccount account) {
-        if (accountRepository.findById(account.getId()).isPresent()){
+        if (accountRepository.findById(account.getId()).isPresent()) {
             return false;
         }
         accountRepository.save(account);
@@ -35,6 +49,52 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public boolean create(AccountDto accountDto) {
         return false;
+    }
+
+    @Override
+    public void save(UserAccount account) {
+        accountRepository.save(account);
+    }
+
+    @Override
+    public Optional<UserAccount> findByEmail(String email) {
+        return accountRepository.findByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public boolean transfer(UserAccount from, UserAccount to, String currency, Double amount) {
+
+        CurrencyCode fromC = from.getCurrencyCode();
+        CurrencyCode toC = to.getCurrencyCode();
+
+        Optional<Currency> optCf = currencyRepository.findById(fromC);
+        Optional<Currency> optCt = currencyRepository.findById(toC);
+        Optional<Currency> ofC = currencyRepository.findById(CurrencyCode.valueOf(currency));
+
+
+        if (optCf.isEmpty() || optCt.isEmpty() || ofC.isEmpty()) {
+            return false;
+        }
+
+        Double fromK = optCf.get().price;
+        Double toK = optCt.get().price;
+        Double ofK = ofC.get().price;
+
+        Double amountT = amount * (ofK/toK);
+        Double amountF = amount * (ofK/fromK);
+
+        if (from.getBalance() < amountF){
+            return false;
+        }
+
+        from.setBalance(from.getBalance() - amountF);
+        to.setBalance(to.getBalance() + amountT);
+
+        accountRepository.save(from);
+        accountRepository.save(to);
+
+        return true;
     }
 
 

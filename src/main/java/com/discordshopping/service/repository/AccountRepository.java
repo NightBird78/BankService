4f02b694ba2752531e9241752ee061f401103487
NightBird78 <panbogdan05@gmail.com>
2023-10-16 18:31:05 +0200

    findBy...

--- src/main/java/com/discordshopping/service/repository/AccountRepository.java
+++ src/main/java/com/discordshopping/service/repository/AccountRepository.java
@@ -2,10 +2,22 @@ package com.discordshopping.service.repository;
 
 import com.discordshopping.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<UserAccount, UUID> {
+
+    @Query("select u from UserAccount u where u.idba = :idba")
+    Optional<UserAccount> findByIDBA(@Param("idba") String idba);
+
+    @Query("select u from UserAccount u where u.user.id = :id")
+    Optional<UserAccount> findByUserId(@Param("id") UUID id);
+
+    @Query("select u from UserAccount u where u.user.email = :email")
+    Optional<UserAccount> findByEmail(@Param("email") String email);
 }

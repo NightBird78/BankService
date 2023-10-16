849b8fe2fcefaddc635144a63bc6dcb67628bfbc
NightBird78 <panbogdan05@gmail.com>
2023-10-16 18:39:12 +0200

    add email

--- src/main/java/com/discordshopping/bot/util/Cookie.java
+++ src/main/java/com/discordshopping/bot/util/Cookie.java
@@ -3,10 +3,13 @@ package com.discordshopping.bot.util;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
+@ToString
 @Getter
 @Setter
 public class Cookie {
     private String login;
+    private String email;
 }

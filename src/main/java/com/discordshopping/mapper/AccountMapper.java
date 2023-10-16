ac7a764661ff33633c418d949a5668c58a2ec163
NightBird78 <panbogdan05@gmail.com>
2023-10-16 18:32:02 +0200

    merge

--- src/main/java/com/discordshopping/mapper/AccountMapper.java
+++ src/main/java/com/discordshopping/mapper/AccountMapper.java
@@ -2,10 +2,13 @@ package com.discordshopping.mapper;
 
 import com.discordshopping.entity.UserAccount;
 import com.discordshopping.entity.dto.AccountDto;
+import com.discordshopping.entity.dto.AccountUpdatedDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AccountMapper {
     @Mapping(source = "user.nickName", target ="nickName")
     @Mapping(source = "accountStatus", target = "status")
@@ -14,4 +17,7 @@ public interface AccountMapper {
     @Mapping(source = "createdAt", target = "date")
     AccountDto accountToDto(UserAccount account);
 
+    UserAccount merge(UserAccount from, @MappingTarget UserAccount to);
+
+    UserAccount dtoToAccount(AccountUpdatedDto accountUpdatedDto);
 }
--- src/main/java/com/discordshopping/mapper/UserMapper.java
+++ src/main/java/com/discordshopping/mapper/UserMapper.java
@@ -2,12 +2,21 @@ package com.discordshopping.mapper;
 
 import com.discordshopping.entity.User;
 import com.discordshopping.entity.dto.UserDto;
+import com.discordshopping.entity.dto.UserUpdatedDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
     User dtoToUser(UserDto dto);
 
+    User dtoToUser(UserUpdatedDto user);
+
     UserDto userToDto(User user);
+
+    User merge(User from,@MappingTarget User to);
+
+    UserUpdatedDto fullDto(User user);
 }

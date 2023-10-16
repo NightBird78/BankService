744be289b934a6bb26b76e8a5872e4cbc01b4891
NightBird78 <panbogdan05@gmail.com>
2023-10-16 18:38:48 +0200

    uuid validator

--- src/main/java/com/discordshopping/bot/util/MiniUtil.java
+++ src/main/java/com/discordshopping/bot/util/MiniUtil.java
@@ -1,8 +1,12 @@
 package com.discordshopping.bot.util;
 
+import com.github.javafaker.Faker;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MiniUtil {
@@ -11,5 +15,24 @@ public class MiniUtil {
         MessageDigest md = MessageDigest.getInstance("SHA256");
 
         return UUID.nameUUIDFromBytes(md.digest(name.getBytes()));
+    }
+
+    public static boolean isValidUUID(String id){
+        Pattern pattern = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}");
+        Matcher matcher = pattern.matcher(id);
+
+        if(matcher.find()) {
+            return matcher.group().equals(id);
+        }
+        return false;
+    }
+
+    public static String createBankIdentifier(){
+        Faker faker = new Faker();
+        String number = faker.regexify("[0-9]{16}");
+
+        String primaryCode = "IDBA";
+
+        return primaryCode+number;
     }
 }
